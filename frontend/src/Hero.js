8916f739e4b2d41c77f68acde2df88327ee375e4
Robert Inez <rinez@cnm.edu>
2021-07-23 22:25:54 -0600

    added hero image.

--- frontend/src/Hero.js
+++ frontend/src/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ParticleBackground} from "./ParticleBackground";
 import {CenterTitle} from './CenterTitle';
+import HeroImg from './images/hero-bg-1.jpg'
 import './Hero.css'
 
 
@@ -8,6 +9,7 @@ export const Hero = () => {
 	return (
 		<>
 			<div className='hero'>
+				<img src = {HeroImg} alt = 'HeroImage' />
 				{/*<ParticleBackground />*/}
 				<CenterTitle />
 			</div>
